1c2851b114d2eee67559e2520391e99927d4f5f6
oscarnar <orosas58@gmail.com>
2020-05-21 12:22:30 -0500

    Agregue compute para el procesamiento

--- lib/algorithms/operLogaritmo.dart
+++ lib/algorithms/operLogaritmo.dart
@@ -1,26 +1,36 @@
 import 'dart:io';
 import 'dart:math';
+import 'package:flutter/foundation.dart';
 import 'package:image/image.dart' as img;
 import 'package:path_provider/path_provider.dart';
 
-Future<void> operLogaritmo({int c, File imageFile, String name}) async {
+class Log{
+  img.Image imagen;
+  int c;
+  Log(this.imagen, this.c);
+}
 
-    img.Image ori = img.decodeImage(imageFile.readAsBytesSync());
-    int wi = ori.width;
-    int he = ori.height;
-
-    for (int x = 0; x < wi; x++) {
-      for (int y = 0; y < he; y++) {
-        int red = img.getRed(ori[y * wi + x]);
-        double newColor = c * (log(red)/ln10);
-        if (newColor < 0)
-          newColor = 0;
-        if(newColor > 255)
-          newColor = 255;
-        ori.setPixelRgba(x, y, newColor.toInt(), newColor.toInt(), newColor.toInt());
-      }
+Future<img.Image> Logaritmo(Log data) async {
+  int wi = data.imagen.width;
+  int he = data.imagen.height;
+  for (int x = 0; x < wi; x++) {
+    for (int y = 0; y < he; y++) {
+      int red = img.getRed(data.imagen[y * wi + x]);
+      double newColor = data.c * (log(red) / ln10);
+      if (newColor < 0) newColor = 0;
+      if (newColor > 255) newColor = 255;
+      data.imagen.setPixelRgba(
+          x, y, newColor.toInt(), newColor.toInt(), newColor.toInt());
     }
-    
-    final directory = await getApplicationDocumentsDirectory();
-    File('${directory.path}/$name.jpg').writeAsBytesSync(img.encodeJpg(ori));
   }
+  return data.imagen;
+}
+
+Future<void> operLogaritmo({int c, File imageFile, String name}) async {
+  img.Image ori = img.decodeImage(imageFile.readAsBytesSync());
+
+  ori = await compute(Logaritmo, Log(ori,c));
+
+  final directory = await getApplicationDocumentsDirectory();
+  File('${directory.path}/$name.jpg').writeAsBytesSync(img.encodeJpg(ori));
+}
